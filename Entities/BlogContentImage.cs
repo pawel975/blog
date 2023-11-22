549ccc6a4bc3d7fab6643cd0ba6ed8c338ab7b2b
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-22 20:39:01 +0100

    basic entities for entity framework

---
+++ Entities/BlogContentImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Entities
+{
+    public class BlogContentImage
+    {
+        public int Id { get; set; }
+        public string ImageSrc { get; set; }
+        public string AltTitle { get; set; }
+    }
+}
---
+++ Entities/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Blog.Entities
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string PrimaryImageSrc { get; set; }
+        public string BlogPostContent { get; set; }
+        public List<BlogContentImage> BlogContentImages { get; set; }
+
+    }
+}
