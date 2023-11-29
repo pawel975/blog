bf89e07ccdf85b2e4714a4d6685a0d227bcbccf3
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-29 23:26:54 +0100

    captured connection string from appsettings.json

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BlogDbContext>();
 
+// Added connection string
+builder.Configuration.GetConnectionString("DefaultConnection");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
