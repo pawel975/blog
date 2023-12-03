9df439ef52593f4ca06fa134b9c02f2054449b88
Paweł Kurek <pawel.kurek975@gmail.com>
2023-12-03 22:42:32 +0100

    added create request to api of blog

---
+++ Models/CreateBlogPostDto.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class CreateBlogPostDto
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ShortDescription { get; set; }
+        public string PrimaryImageSrc { get; set; }
+        [Required]
+        public string BlogPostContent { get; set; }
+        public List<BlogContentImage>? BlogContentImages { get; set; }
+    }
+}
--- Services/BlogPostService.cs
+++ Services/BlogPostService.cs
@@ -1,19 +1,24 @@
-﻿using Blog.Entities;
+﻿using AutoMapper;
+using Blog.Entities;
+using Blog.Models;
 
 namespace Blog.Services
 {
     public interface IBlogPostService
     {
+        int Create(CreateBlogPostDto dto);
         List<BlogPost> GetAllBlogPosts();
         BlogPost GetBlogPostById(int id);
     }
 
     public class BlogPostService : IBlogPostService
     {
+        private readonly IMapper _mapper;
         private readonly BlogDbContext _dbContext;
 
-        public BlogPostService(BlogDbContext dbContext)
+        public BlogPostService(BlogDbContext dbContext, IMapper mapper)
         {
+            _mapper = mapper;
             _dbContext = dbContext;
         }
 
@@ -29,6 +34,17 @@ namespace Blog.Services
             BlogPost blogPost = _dbContext.BlogPosts.FirstOrDefault(bp => bp.Id == id);
 
             return blogPost;
+        }
+
+        public int Create(CreateBlogPostDto dto)
+        {
+            var blogPost = _mapper.Map<BlogPost>(dto);
+
+            _dbContext.BlogPosts.Add(blogPost);
+            _dbContext.SaveChanges();
+
+            return blogPost.Id;
+
         }
     }
 }
