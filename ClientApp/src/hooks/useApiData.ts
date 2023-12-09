849b345cf7ae3a8faea338aa9035d1ed1ab7c56f
Paweł Kurek <pawel.kurek975@gmail.com>
2023-12-09 19:14:15 +0100

    custom api fetch hook

---
+++ ClientApp/src/hooks/useApiData.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+interface ApiReturnedElementsInterface {
+  data: object | null;
+  loading: boolean;
+  error: Error | null;
+}
+
+const useApiData = (url: string): ApiReturnedElementsInterface => {
+  const [data, setData] = useState<object | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(
+          error instanceof Error ? error : new Error("An error occured")
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useApiData;
