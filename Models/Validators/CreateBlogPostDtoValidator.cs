06b683f0080f8fdb5c16f5aa903ea81a9580a606
Paweł Kurek <pawel.kurek975@gmail.com>
2023-12-16 18:47:50 +0100

    update validation of created post

---
+++ Models/Validators/CreateBlogPostDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Entities;
+using FluentValidation;
+
+namespace Blog.Models.Validators
+{
+    public class CreateBlogPostDtoValidator : AbstractValidator<CreateBlogPostDto>
+    {
+        public CreateBlogPostDtoValidator(BlogDbContext dbContext)
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(100);
+
+            RuleFor(x => x.ShortDescription)
+                .NotEmpty()
+                .MinimumLength(30)
+                .MaximumLength(200);
+
+            RuleFor(x => x.BlogPostContent)
+                .NotEmpty()
+                .MinimumLength(100)
+                .MaximumLength(10000);
+
+
+        }
+    }
+}
