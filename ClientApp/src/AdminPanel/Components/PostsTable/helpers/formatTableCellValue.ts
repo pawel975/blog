04011e949e3d5c5e7eec54c8540ce3f80966e782
Paweł Kurek <pawel.kurek975@gmail.com>
2023-12-30 00:55:41 +0100

    refactr of posts table

---
+++ ClientApp/src/AdminPanel/Components/PostsTable/helpers/formatTableCellValue.ts
@@ -0,0 +1,9 @@
+const formatTableCellValue = (val: string | undefined | null, trimToCharLength: number = 40): string => {
+  return val
+    ? val.length > trimToCharLength
+      ? val.slice(0, trimToCharLength) + "..."
+      : val.slice(0, trimToCharLength)
+    : "N/A";
+};
+
+export default formatTableCellValue;
--- ClientApp/src/AdminPanel/Layout/AdminNavMenu/AdminNavMenuRoutes.ts
+++ ClientApp/src/AdminPanel/Layout/AdminNavMenu/AdminNavMenuRoutes.ts
@@ -1,10 +1,6 @@
 type AdminNavMenuRoute = "dashboard" | "posts" | "create";
 
-const adminNavMenuRoutes: AdminNavMenuRoute[] = [
-  "dashboard",
-  "posts",
-  "create",
-];
+const adminNavMenuRoutes: AdminNavMenuRoute[] = ["dashboard", "posts", "create"];
 
 export default AdminNavMenuRoute;
 export { adminNavMenuRoutes };
