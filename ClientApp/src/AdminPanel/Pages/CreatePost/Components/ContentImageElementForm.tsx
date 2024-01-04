eb862e0943e6ddaa69d64b8ba33141c8bfc65988
Paweł Kurek <pawel.kurek975@gmail.com>
2024-01-04 21:49:06 +0100

    changed structure of client side

---
+++ ClientApp/src/AdminPanel/Pages/CreatePost/Components/ContentImageElementForm.tsx
@@ -0,0 +1,7 @@
+import FormLayout from "./FormLayout";
+
+const ContentImageElementForm: React.FC = () => {
+  return <FormLayout></FormLayout>;
+};
+
+export default ContentImageElementForm;
---
+++ ClientApp/src/AdminPanel/Pages/CreatePost/Components/ParagraphElementForm.tsx
@@ -0,0 +1,7 @@
+import FormLayout from "./FormLayout";
+
+const ParagraphElementForm: React.FC = () => {
+  return <FormLayout></FormLayout>;
+};
+
+export default ParagraphElementForm;
