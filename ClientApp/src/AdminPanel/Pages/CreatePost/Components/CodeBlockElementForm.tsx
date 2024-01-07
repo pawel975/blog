42b01d247fc39329340d49856aadda4afb10e4aa
Paweł Kurek <pawel.kurek975@gmail.com>
2024-01-07 19:54:36 +0100

    basic content elements creating mechanism without data validation

--- ClientApp/src/AdminPanel/Pages/CreatePost/Components/CodeBlockElementForm.tsx
+++ ClientApp/src/AdminPanel/Pages/CreatePost/Components/CodeBlockElementForm.tsx
@@ -1,7 +1,76 @@
-import FormLayout from "./FormLayout";
+import { useState } from "react";
+import { CodeBlock } from "../../../../common/types";
+import { ContentElements } from "../types";
+import { Button, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
 
-const CodeBlockElementForm: React.FC = () => {
-  return <FormLayout></FormLayout>;
+interface CodeBlockElementFormProps {
+  codeBlocksErrors: string[];
+  setContentElements: Function;
+}
+
+const initCodeBlockState: CodeBlock = {
+  content: "",
+  language: "js",
+};
+
+const codeBlockLanguages: CodeBlock["language"][] = ["js", "cs", "html", "css"];
+
+const CodeBlockElementForm: React.FC<CodeBlockElementFormProps> = ({ codeBlocksErrors, setContentElements }) => {
+  const [codeBlockState, setCodeBlockState] = useState<CodeBlock>(initCodeBlockState);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setContentElements((prevState: ContentElements) => ({
+      ...prevState,
+      codeBlocks: [...prevState.codeBlocks, codeBlockState],
+    }));
+
+    if (codeBlocksErrors.length === 0) setCodeBlockState(initCodeBlockState);
+  };
+
+  const allCodeBlockLanguages = codeBlockLanguages.map((lang) => <option value={lang}>{lang}</option>);
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="content">Content</Label>
+        <Input
+          className="mb-2"
+          invalid={Boolean(codeBlocksErrors.length > 0)}
+          id="content"
+          type="text"
+          name="content"
+          value={codeBlockState.content}
+          onChange={(e) => setCodeBlockState((prevState) => ({ ...prevState, content: e.target.value }))}
+        />
+        {codeBlocksErrors.map((errorMsg, index) => (
+          <FormFeedback key={index}>{errorMsg}</FormFeedback>
+        ))}
+      </FormGroup>
+
+      <FormGroup>
+        <Label for="language">Language</Label>
+        <Input
+          className="mb-2"
+          invalid={Boolean(codeBlocksErrors.length > 0)}
+          id="language"
+          type="select"
+          name="language"
+          value={codeBlockState.language}
+          onChange={(e) =>
+            setCodeBlockState((prevState) => ({ ...prevState, language: e.target.value as CodeBlock["language"] }))
+          }
+        >
+          {allCodeBlockLanguages}
+        </Input>
+
+        {codeBlocksErrors.map((errorMsg, index) => (
+          <FormFeedback key={index}>{errorMsg}</FormFeedback>
+        ))}
+      </FormGroup>
+      <Button color="info">Add element</Button>
+    </Form>
+  );
 };
 
 export default CodeBlockElementForm;
--- ClientApp/src/AdminPanel/Pages/CreatePost/Components/FormLayout.tsx
+++ ClientApp/src/AdminPanel/Pages/CreatePost/Components/FormLayout.tsx
@@ -1,34 +1,9 @@
-import { Form } from "reactstrap";
-import { Button, FormGroup, Input, Label } from "reactstrap";
-
 type FormLayoutProps = {
   children: React.ReactNode;
 };
 
 const FormLayout: React.FC<FormLayoutProps> = ({ children }) => {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="content">Content</Label>
-        <Input
-          className="mb-2"
-          // TODO: Implement validation
-          // invalid={Boolean(errors["Title"].length > 0)}
-          id="content"
-          type="text"
-          name="content"
-          // value={title}
-          // onChange={(e) => setTitle(e.target.value)}
-        />
-        {children}
-        {/* TODO: Implement validation */}
-        {/* {errors["Title"].map((errorMsg, index) => (
-            <FormFeedback key={index}>{errorMsg}</FormFeedback>
-          ))} */}
-      </FormGroup>
-      <Button color="info">Add element</Button>
-    </Form>
-  );
+  return <>{children}</>;
 };
 
 export default FormLayout;
