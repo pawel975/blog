0d0f95fde86452fba6166b43069490573c2ad952
Paweł Kurek <pawel.kurek975@gmail.com>
2024-01-07 10:09:15 +0100

    removed app.settings.Development.json from repository

--- Program.cs
+++ Program.cs
@@ -38,13 +38,13 @@ var seeder = scope.ServiceProvider.GetRequiredService<BlogSeeder>();
 app.UseResponseCaching();
 seeder.Seed();
 
-app.UseSwagger();
-app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
